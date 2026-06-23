3e074896ff72fd4ca3d2572e5505b6d14b9be321
Meet Shah <warmessgaming@gmail.com>
2026-06-23 16:43:12 +0530

    Add files

---
+++ Day1/include/add.h
@@ -0,0 +1,8 @@
+#ifndef ADD_TMP
+    #define ADD_TMP
+    int add(int a,int b);
+    double add(double a,double b);
+    float add(float a,float b);
+    char add(char a,char b);
+    unsigned int add(unsigned int a, unsigned int b);
+#endif
---
+++ Day1/src/add.cxx
@@ -0,0 +1,20 @@
+#include <add.h>
+
+int add(int a,int b){
+    return a+b;
+}
+double add(double a,double b){
+    return a+b;
+}
+
+float add(float a,float b){
+    return a+b;
+}
+
+char add(char a,char b){
+    return a+b;
+}
+
+unsigned int add(unsigned int a, unsigned int b){
+    return a+b;
+}
